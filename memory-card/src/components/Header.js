f4251e7b60d6bf3f62b9f37010ce2a2a8265c685
zephyr96 <aaadev32@gmail.com>
2023-01-24 13:31:37 -0800

    updated header to display the correct amount of unique selections to win the game

--- memory-card/src/components/Header.js
+++ memory-card/src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
         <div id="header">
             <div id="header-info">
                 <h1 id="header-title">Animal Memory Card Game</h1>
-                <p id="header-instructions">test your working memory in this memory card game, each time you choose a card the catalogue will be scrambled. try to get 10 unique selections to win the game!</p>
+                <p id="header-instructions">test your working memory in this memory card game, each time you choose a card the catalogue will be scrambled. try to get 12 unique selections to win the game!</p>
             </div>
             <div id="header-score">
                 <div id="score">Score: 0</div>
